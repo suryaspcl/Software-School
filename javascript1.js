322d97bf6a38eb641f13569b37f55766077a85cf
suryspcl <sury295.spcl@gmail.com>
2024-10-13 22:43:30 +0530

    javascript-1

---
+++ javascript1.js
@@ -0,0 +1,23 @@
+/* Task-1*/
+var userName="surya";
+var userEmail="surya@gmail.com";
+var userNumber="9989532410";
+var userPassword="Adcr@#123";
+var userAddress="D-No:1-63,Madhapur,Hyderabad";
+var userPincode="500081";
+var userMonthlySalary="30000";
+var userAnnualSalaryPackage="3,60000";
+
+console.log(userName)
+console.log(userEmail)
+console.log(userNumber)
+console.log(userPassword)
+console.log(userAddress)
+console.log(userPincode)
+console.log(userMonthlySalary)
+console.log(userAnnualSalaryPackage)
+
+/* Task-2*/
+
+
+
