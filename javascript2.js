24df30d7c3b8845a667316574d8d43f626a5c0eb
suryspcl <sury295.spcl@gmail.com>
2024-10-16 20:41:34 +0530

    javascript changes

---
+++ javascript2.js
@@ -0,0 +1,12 @@
+
+// javascript-2 //
+
+var userName = "surya";
+
+console.log(userName.length);
+
+var userName = "suryachandra"
+
+var noOfInCharsUserName = userName.length;
+
+console.log(noOfCharsUserName);
